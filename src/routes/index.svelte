bb3e7b825d38b0834b6644e0d653d6d1d21a2a4e
Andrew <aheller9@gmail.com>
2022-02-16 17:29:19 -0600

    title

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,6 @@
+<svelte:head>
+	<title>Svelte Board- Svelte Tutorials, Code Snippets, and Svmore!</title>
+</svelte:head>
+
 <h1 class="text-center mt-12 text-4xl font-black">Welcome to Svelte Board</h1>
 <p class="text-lg text-center">Tutorials, Code Snippets, and Svmore coming soon!</p>
