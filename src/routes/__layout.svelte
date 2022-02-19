71f607c692f4083896df342e8ef542596b7fb166
Andrew <aheller9@gmail.com>
2022-02-19 15:59:03 -0600

    sync changes

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -191,6 +191,26 @@
 					>
 				</a>
 			</li>
+			<li class="flex items-stretch space-x-1 hover:bg-slate-200 rounded-md group">
+				<a href="https://github.com/svelteboard/svelteboard" class="flex w-full p-2">
+					<span class="flex items-center justify-center mr-2">
+						<svg
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+							stroke-width="2"
+							fill="none"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							class="h-6 w-6 2xl:h-8 2xl:w-8 2xl:p-1 text-slate-500 group-hover:text-slate-700"
+							><path
+								d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
+							/></svg
+						>
+					</span><span class="flex-1 text-slate-700 group-hover:text-slate-900 py-1 ml-1"
+						>Github</span
+					>
+				</a>
+			</li>
 		</ul>
 	</nav>
 
