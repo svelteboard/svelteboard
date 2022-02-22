b69c38476da70be52840d6221814f9dc8ad0d74d
Andrew <aheller9@gmail.com>
2022-02-22 15:55:57 -0600

    package cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,4 @@
-import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-auto';
-//import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
