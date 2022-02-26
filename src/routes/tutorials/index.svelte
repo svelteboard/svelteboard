659b85a0dedc199a4f4212739bda67c965ccbc92
Andrew <aheller9@gmail.com>
2022-02-26 09:32:13 -0600

    remove svg/description

--- src/routes/tutorials/index.svelte
+++ src/routes/tutorials/index.svelte
@@ -9,7 +9,6 @@
 <div class="max-w-7xl m-auto pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
 	<div>
 		<h2 class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">Tutorials</h2>
-		<p class="mt-3 text-xl text-gray-500 sm:mt-4">Start your side project</p>
 	</div>
 	<div class="relative max-w-lg mx-auto divide-y-2 divide-gray-200 lg:max-w-7xl">
 		<div class="mt-12 grid gap-16 pt-12 lg:grid-cols-3 lg:gap-x-5 lg:gap-y-12">
