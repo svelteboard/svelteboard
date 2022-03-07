ec6f8c53b7380aa18109033b941b787f4faa655e
Andrew <aheller9@gmail.com>
2022-03-07 15:11:47 -0600

    cors test

--- src/routes/contact.js
+++ src/routes/contact.js
@@ -1,14 +1,31 @@
-const pipedreamForm = import.meta.env.VITE_PIPEDREAM_CONTACT;
+// export async function post({ request }) {
+// 	const res = await request.json();
+// 	//const headers = new Headers();
+// 	//	headers.append('Content-Type', 'application/json');
+// 	const corsHeaders = {
+// 		'Content-Type': 'text/JSON',
+// 		'Access-Control-Allow-Headers': '*',
+// 		'Access-Control-Allow-Methods': 'POST',
+// 		'Access-Control-Allow-Origin': '86400'
+// 	};
+
+// 	const options = {
+// 		method: 'POST',
+// 		headers: corsHeaders,
+// 		body: JSON.stringify(res)
+// 	};
+// 	fetch('https://eo7kwhtkijw77n6.m.pipedream.net', options);
+
+// 	return { body: { status: '200' } };
+// }
 
 export async function post({ request }) {
 	const res = await request.json();
-	//const headers = new Headers();
-	//	headers.append('Content-Type', 'application/json');
 	const corsHeaders = {
 		'Content-Type': 'text/JSON',
 		'Access-Control-Allow-Headers': '*',
-		'Access-Control-Allow-Methods': '*',
-		'Access-Control-Allow-Origin': '*'
+		'Access-Control-Allow-Methods': 'POST',
+		'Access-Control-Allow-Origin': '86400'
 	};
 
 	const options = {
@@ -16,7 +33,9 @@ export async function post({ request }) {
 		headers: corsHeaders,
 		body: JSON.stringify(res)
 	};
-	fetch('https://eo7kwhtkijw77n6.m.pipedream.net', options);
 
+	const pipedreamForm = import.meta.env.VITE_PIPEDREAM_CONTACT;
+
+	const pipe = await fetch('https://eo7kwhtkijw77n6.m.pipedream.net', options);
 	return { body: { status: '200' } };
 }
