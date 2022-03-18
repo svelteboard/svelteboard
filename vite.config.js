774183927b5a642759bbded979fb78cee5553b5e
andheller <aheller9@gmail.com>
2022-03-18 11:54:16 -0500

    Polyfill test

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+const config = {
+	resolve: {
+		alias: {
+			buffer: 'buffer',
+			crypto: 'crypto-browserify',
+			events: 'events/',
+			path: 'path-browserify',
+			stream: 'stream-browserify'
+		}
+	}
+};
+
+export default config;
