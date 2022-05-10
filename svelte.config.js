16f6b70ee7d046a1ac4bc0c953bead70afc3b279
andheller <aheller9@gmail.com>
2022-05-10 06:09:47 -0500

    npm

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
 import preprocess from 'svelte-preprocess';
-//import adapter from '@sveltejs/adapter-auto';
 import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
